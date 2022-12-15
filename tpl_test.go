e78779d5bd7a41f5b19b356d5f348bc6d8fdb264
pchchv <ipchchv@gmail.com>
2022-12-15 17:51:53 +0300

    Implement a New funcs tests

---
+++ tpl_test.go
@@ -0,0 +1,57 @@
+package tpl
+
+import (
+	"fmt"
+	"testing"
+)
+
+const (
+	testString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+		"Etiam ac convallis risus. Ut accumsan urna sem, in placerat mi luctus a. " +
+		"Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; " +
+		"Morbi eu massa in nulla rutrum maximus vitae id massa. Aenean venenatis, nunc nec cursus porta, ex lorem egestas erat, ut."
+
+	testFile = "./testText.txt"
+)
+
+func TestNewFromSting(t *testing.T) {
+	text, err := NewFromString(testString)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if fmt.Sprintf("%T", text) != "*tpl.Text" {
+		t.Fatal("Incorrect type")
+	}
+
+	if len(text.Text) <= 1 {
+		t.Fatal("Text split error")
+	}
+}
+
+func TestWrongNewFromString(t *testing.T) {
+	_, err := NewFromString("Hello")
+	if err == nil {
+		t.Fatal("No error when getting a word")
+	}
+
+	_, err = NewFromString("")
+	if err == nil {
+		t.Fatal("No error when getting a empty input")
+	}
+}
+
+func TestNewFromFile(t *testing.T) {
+	text, err := NewFromFile(testFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if fmt.Sprintf("%T", text) != "*tpl.Text" {
+		t.Fatal("Incorrect type")
+	}
+
+	if len(text.Text) <= 1 {
+		t.Fatal("Text split error")
+	}
+}
